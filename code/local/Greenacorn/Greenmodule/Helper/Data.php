a02d4a74cc017c55f4c8e66c76821dc0a02f1a14
clean web <cleanweb11@gmail.com>
2015-10-27 00:04:13 +0530

    new magento

---
+++ code/local/Greenacorn/Greenmodule/Helper/Data.php
@@ -0,0 +1,3 @@
+<?php
+class Greenacorn_Greenmodule_Helper_Data extends Mage_Core_Helper_Abstract
+{}
---
+++ code/local/Greenacorn/Greenmodule/controllers/Adminhtml/IndexController.php
@@ -0,0 +1,13 @@
+<?php 
+class Greenacorn_Greenmodule_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action
+{
+  public function indexAction()
+  {
+           $this->loadLayout();
+         
+        $block = $this->getLayout()->createBlock('core/text', 'green-block')->setText('<h1>Green Acorn Web Design</h1>');
+        $this->_addContent($block);
+         
+        $this->_setActiveMenu('green_menu')->renderLayout(); 
+  }
+}
